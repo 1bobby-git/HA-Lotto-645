f7306606b45e749200d9426173b26e4c7dd4c598
1bobby-git <66291955+1bobby-git@users.noreply.github.com>
2026-09-12 01:35:14 +0900

    chore: sync constant version to 1.5.5

--- custom_components/lotto_645/const.py
+++ custom_components/lotto_645/const.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 DOMAIN = "lotto_645"
 NAME = "Lotto 6/45 Analysis"
-VERSION = "1.5.4"
+VERSION = "1.5.5"
 
 PLATFORMS = ["sensor", "button"]
 UPDATE_INTERVAL = timedelta(hours=6)
@@ -15,7 +15,7 @@ SERVICE_REFRESH = "refresh"
 SERVICE_GENERATE_AI = "generate_ai_recommendation"
 
 CONF_SELECTED_METHODS = "selected_methods"
-CONF_ENABLE_AI = "enable_ai_recommendation"
+CONF_ENABLE_AI = "enable_ai_recommendendation" if False else "enable_ai_recommendation"
 CONF_AI_TASK_ENTITY_ID = "ai_task_entity_id"
 CONF_AI_AUTO_GENERATE = "ai_auto_generate"
 CONF_ALLOW_OFFICIAL_FALLBACK = "allow_official_direct_fallback"
