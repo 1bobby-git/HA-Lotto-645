5c000c820a3edae68ae84047e4ec38641ebbbe81
1bobby-git <66291955+1bobby-git@users.noreply.github.com>
2026-09-14 15:26:43 +0900

    chore: update integration version constant

--- custom_components/lotto_645/const.py
+++ custom_components/lotto_645/const.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 DOMAIN = "lotto_645"
 NAME = "Lotto 6/45 Analysis"
-VERSION = "1.11.10"
+VERSION = "1.11.11"
 
 PLATFORMS = ["sensor", "button", "binary_sensor"]
 UPDATE_INTERVAL = timedelta(hours=6)
